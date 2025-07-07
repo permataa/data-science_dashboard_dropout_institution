05196e9b02fe9f4625a7d170f1c5c2e5a56af1ce
Permata Ayu <87806728+permataa@users.noreply.github.com>
2025-07-07 22:19:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def load_model():
     feature_columns = joblib.load('model/feature_columns.pkl')
     return model, feature_columns
     
+model, feature_columns = load_model()
 
 # UI Utama
 st.title("🎓 Prediksi Risiko Dropout Mahasiswa")
